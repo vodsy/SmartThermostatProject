478f0260de5a954d9674b5fd41bb20412ed368da
Jonathan Handy <57910664+vodsy@users.noreply.github.com>
2021-04-23 09:42:43 -0400

    Initial commit

---
+++ gpiointerrupt_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/gpiointerrupt.d
@@ -0,0 +1,51 @@
+# FIXED
+
+gpiointerrupt.obj: ../gpiointerrupt.c
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdio.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdarg.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/GPIO.h
+gpiointerrupt.obj: /Users/jonathanhandy/workspace_v10/gpiointerrupt_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/syscfg/ti_drivers_config.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/devices/DeviceFamily.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/I2C.h
+gpiointerrupt.obj: /Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdbool.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/dpl/HwiP.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/dpl/SemaphoreP.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/UART.h
+gpiointerrupt.obj: /Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/Timer.h
+
+../gpiointerrupt.c: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdint.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_ti_config.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/linkage.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/_stdint40.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/stdint.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/cdefs.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_types.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_types.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/machine/_stdint.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/sys/_stdint.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stddef.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdio.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdarg.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdlib.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/GPIO.h: 
+/Users/jonathanhandy/workspace_v10/gpiointerrupt_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/syscfg/ti_drivers_config.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/devices/DeviceFamily.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/I2C.h: 
+/Applications/ti/ccs1020/ccs/tools/compiler/ti-cgt-arm_20.2.2.LTS/include/stdbool.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/dpl/HwiP.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/dpl/SemaphoreP.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/UART.h: 
+/Applications/ti/simplelink_cc32xx_sdk_4_40_00_07/source/ti/drivers/Timer.h: 
---
+++ gpiointerrupt_CC3220S_LAUNCHXL_nortos_ccs/gpiointerrupt.c
@@ -0,0 +1,328 @@
+/*
+ * Copyright (c) 2015-2020, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ======== gpiointerrupt.c ========
+ */
+#include <stdint.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Driver Header files */
+#include <ti/drivers/GPIO.h>
+
+/* Driver configuration */
+#include "ti_drivers_config.h"
+#include <ti/drivers/I2C.h>
+#include <ti/drivers/UART.h>
+#include <ti/drivers/Timer.h>
+
+
+
+
+#define DISPLAY(x) UART_write(uart, &output, x);
+
+// Global variables
+volatile unsigned char TimerFlag = 0;
+volatile unsigned char TempUpFlag = 0;
+volatile unsigned char TempDownFlag = 0;
+
+// UART Global Variables
+char output[64];
+int bytesToSend;
+
+// Driver Handles - Global variables
+UART_Handle uart;
+void initUART(void) {
+    UART_Params uartParams;
+
+    // Init the driver
+    UART_init();
+
+    // Configure the driver
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_BINARY;
+    uartParams.readDataMode = UART_DATA_BINARY;
+    uartParams.readReturnMode = UART_RETURN_FULL;
+    uartParams.baudRate = 115200;
+
+    // Open the driver
+    uart = UART_open(CONFIG_UART_0, &uartParams);
+    if (uart == NULL) {
+        /* UART_open() failed */
+        while (1) {};
+    }
+}
+
+// I2C Global Variables
+static const struct {
+    uint8_t address;
+    uint8_t resultReg;
+    char *id;
+} sensors[3] = {
+                { 0x48, 0x0000, "11X" },
+                { 0x49, 0x0000, "116" },
+                { 0x41, 0x0001, "006" }
+};
+uint8_t txBuffer[1];
+uint8_t rxBuffer[2];
+I2C_Transaction i2cTransaction;
+
+// Driver Handles - Global variables
+I2C_Handle i2c;
+
+// Make sure you call initUART() before calling this function.
+void initI2C(void) {
+    int8_t i, found;
+    I2C_Params i2cParams;
+    DISPLAY(snprintf(output, 64, "Initializing I2C Driver - "))
+
+    // Init the driver
+    I2C_init();
+
+    // Configure the driver
+    I2C_Params_init(&i2cParams);
+    i2cParams.bitRate = I2C_400kHz;
+
+    // Open the driver
+    i2c = I2C_open(CONFIG_I2C_0, &i2cParams);
+    if (i2c == NULL) {
+        DISPLAY(snprintf(output, 64, "Failed\n\r"))
+        while (1);
+    }
+    DISPLAY(snprintf(output, 32, "Passed\n\r"))
+
+    // Boards were shipped with different sensors.
+    // Welcome to the world of embedded systems.
+    // Try to determine which sensor we have.
+    // Scan through the possible sensor addresses
+    /* Common I2C transaction setup */
+    i2cTransaction.writeBuf = txBuffer;
+    i2cTransaction.writeCount = 1;
+    i2cTransaction.readBuf = rxBuffer;
+    i2cTransaction.readCount = 0;
+    found = false;
+    for (i=0; i<3; ++i) {
+        i2cTransaction.slaveAddress = sensors[i].address;
+        txBuffer[0] = sensors[i].resultReg;
+        DISPLAY(snprintf(output, 64, "Is this %s? ", sensors[i].id))
+        if (I2C_transfer(i2c, &i2cTransaction)) {
+            DISPLAY(snprintf(output, 64, "Found\n\r"))
+            found = true;
+            break;
+        }
+        DISPLAY(snprintf(output, 64, "No\n\r"))
+    }
+    if(found) {
+        DISPLAY(snprintf(output, 64, "Detected TMP%s I2C address: %x\n\r", sensors[i].id, i2cTransaction.slaveAddress))
+    }
+    else {
+        DISPLAY(snprintf(output, 64, "Temperature sensor not found, contact professor\n\r"))
+    }
+}
+
+int16_t readTemp(void) {
+    int16_t temperature = 0;
+    i2cTransaction.readCount = 2;
+    if (I2C_transfer(i2c, &i2cTransaction)) {
+        /*
+         * Extract degrees C from the received data;
+         * see TMP sensor datasheet
+         */
+        temperature = (rxBuffer[0] << 8) | (rxBuffer[1]);
+        temperature *= 0.0078125;
+        /*
+         * If the MSB is set '1', then we have a 2's complement
+         * negative value which needs to be sign extended
+         */
+        if (rxBuffer[0] & 0x80) {
+            temperature |= 0xF000;
+        }
+    }
+    else {
+        DISPLAY(snprintf(output, 64, "Error reading temperature sensor (%d)\n\r",i2cTransaction.status))
+        DISPLAY(snprintf(output, 64, "Please power cycle your board by unplugging USB and plugging back in.\n\r"))
+    }
+    return temperature;
+}
+
+// Driver Handles - Global variables
+Timer_Handle timer0;
+
+void timerCallback(Timer_Handle myHandle, int_fast16_t status) {
+    TimerFlag = 1;
+}
+
+void initTimer(void) {
+    Timer_Params params;
+
+    // Init the driver
+    Timer_init();
+
+    // Configure the driver
+    Timer_Params_init(&params);
+    params.period = 100000; // Timer period set to 100ms
+    params.periodUnits = Timer_PERIOD_US;
+    params.timerMode = Timer_CONTINUOUS_CALLBACK;
+    params.timerCallback = timerCallback;
+
+    // Open the driver
+    timer0 = Timer_open(CONFIG_TIMER_0, &params);
+    if (timer0 == NULL) {
+        /* Failed to initialized timer */
+        while (1) {}
+    }
+
+    if (Timer_start(timer0) == Timer_STATUS_ERROR) {
+        /* Failed to start timer */
+        while (1) {}
+    }
+}
+
+
+/*
+ *  ======== gpioButtonFxn0 ========
+ *  Callback function for the GPIO interrupt on CONFIG_GPIO_BUTTON_0.
+ *
+ *  Note: GPIO interrupts are cleared prior to invoking callbacks.
+ */
+void gpioButtonFxn0(uint_least8_t index)
+{
+    TempDownFlag = 1;
+}
+
+/*
+ *  ======== gpioButtonFxn1 ========
+ *  Callback function for the GPIO interrupt on CONFIG_GPIO_BUTTON_1.
+ *  This may not be used for all boards.
+ *
+ *  Note: GPIO interrupts are cleared prior to invoking callbacks.
+ */
+void gpioButtonFxn1(uint_least8_t index)
+{
+    TempUpFlag = 1;
+}
+
+
+/*
+ *  ======== mainThread ========
+ */
+void *mainThread(void *arg0)
+{
+    /* Call driver init functions */
+    GPIO_init();
+
+    /* Configure the LED and button pins */
+    GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
+    GPIO_setConfig(CONFIG_GPIO_BUTTON_0, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
+
+    /* Install Button callback */
+    GPIO_setCallback(CONFIG_GPIO_BUTTON_0, gpioButtonFxn0);
+
+    /* Enable interrupts */
+    GPIO_enableInt(CONFIG_GPIO_BUTTON_0);
+
+    /*
+     *  If more than one input pin is available for your device, interrupts
+     *  will be enabled on CONFIG_GPIO_BUTTON1.
+     */
+    if (CONFIG_GPIO_BUTTON_0 != CONFIG_GPIO_BUTTON_1) {
+        /* Configure BUTTON1 pin */
+        GPIO_setConfig(CONFIG_GPIO_BUTTON_1, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
+
+        /* Install Button callback */
+        GPIO_setCallback(CONFIG_GPIO_BUTTON_1, gpioButtonFxn1);
+        GPIO_enableInt(CONFIG_GPIO_BUTTON_1);
+    }
+
+    initUART();
+    initI2C();
+    initTimer();
+
+    volatile unsigned int timer = 0;
+    const unsigned long timerPeriod = 100;
+    volatile unsigned char heatFlag; // 0 = heat off, 1 = heat on
+    unsigned long button_elapsedTime;
+    unsigned long temp_elapsedTime;
+    unsigned long print_elapsedTime;
+    volatile char temp = readTemp();
+    volatile char setPoint = 20; // 68F
+
+    GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_OFF);
+
+    while (1) {
+
+        // Check button flags every 200ms
+        if (button_elapsedTime >= 200) { // 200ms have elapsed
+            if (TempDownFlag == 1) {
+                --setPoint;
+                TempDownFlag = 0;
+            }
+            if (TempUpFlag == 1) {
+                ++setPoint;
+                TempUpFlag = 0;
+            }
+            button_elapsedTime = 0;
+        }
+
+        // Check temp and update led every 500ms
+        if (temp_elapsedTime >= 500) { // 500ms have elapsed
+            temp = readTemp();
+            if (temp >= setPoint) { // Room temp is higher than the set point
+                GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_OFF); // Heater is off
+                heatFlag = 0;
+            }
+            else { // Room temp is lower than the set point
+                GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON); // Heater is on
+                heatFlag = 1;
+            }
+            temp_elapsedTime = 0;
+        }
+
+        if (print_elapsedTime >= 1000) { // 1000ms have elapsed
+            timer += 1;
+            DISPLAY(snprintf(output, 64, "<%02d,%02d,%d,%04d>\n\r", temp, setPoint, heatFlag, timer));
+            print_elapsedTime = 0;
+        }
+
+        while (!TimerFlag) {};
+        TimerFlag = 0;
+        button_elapsedTime += timerPeriod;
+        temp_elapsedTime += timerPeriod;
+        print_elapsedTime += timerPeriod;
+
+    }
+}
+
+
+
